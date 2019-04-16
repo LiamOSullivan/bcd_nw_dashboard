62bf772e9034621f35abd22ebed193542aa8560e
LiamOSullivan <lmosulli@tcd.ie>
2019-04-16 20:55:11 +0100

    Add basic culture map

---
+++ public/javascripts/culture_map.js
@@ -0,0 +1,102 @@
+let cultureOSM = new L.TileLayer(stamenTonerUrl_Lite, {
+  minZoom: min_zoom,
+  maxZoom: max_zoom,
+  attribution: stamenTonerAttrib
+});
+
+let cultureMap = new L.Map('culture-map');
+cultureMap.setView(new L.LatLng(nwLat, nwLng), zoom);
+cultureMap.addLayer(cultureOSM);
+
+/************************************
+ * culture
+ ************************************/
+// let cultureCluster = L.markerClusterGroup();
+// let cultureMapIcon = L.icon({
+//   iconUrl: '/images/transport/bicycle-15.svg',
+//   iconSize: [30, 30], //orig size
+//   iconAnchor: [iconAX, iconAY] //,
+//   //popupAnchor: [-3, -76]
+// });
+
+d3.csv("../data/Cultural_Services_and_Facilities.csv")
+  .then(function(data) {
+
+    console.log("Data " + JSON.stringify(data[0]));
+    // console.log("Keys: " + Object.keys(data[0]));
+
+    // let donegalData = data.filter((v) => {
+    //   return v.Region === "Donegal";
+    // });
+    //
+    cultureData = data.map((d) => {
+      let result = proj4(firstProjection, secondProjection, [+d["EASTING"], +d["NORTHING"]]);
+      let marker = L.marker(new L.LatLng(result[1], result[0]))
+      // , {
+      //   icon: waterLevelMapIcon
+      // });
+      marker.bindPopup(d["NAME"]);
+      cultureMap.addLayer(marker);
+    });
+
+    console.log("Mapped data " + JSON.stringify(cultureData[0]));
+
+
+    //
+    // let dcsdcData = data.filter((v) => {
+    //   return v.Region === "DCSDC";
+    // });
+    //
+    // dcsdcData = dcsdcData.map((v) => {
+    //   return {
+    //     Band: v["Age band"],
+    //     Count: parseFloat(v["Count"].replace(/,/g, ''))
+    //   }
+    // });
+    //
+    // // console.log("Mapped DCSDC data " + JSON.stringify(dcsdcData[0]));
+    //
+    // let donegalPopBands = {
+    //   x: donegalData.map((v) => {
+    //     return v.Band;
+    //   }),
+    //   y: donegalData.map((v) => {
+    //     return v.Count;
+    //   }),
+    //   name: 'Donegal',
+    //   orientation: 'v',
+    //   type: 'bar'
+    // };
+    //
+    // let dcsdcPopBands = {
+    //   x: dcsdcData.map((v) => {
+    //     return v.Band;
+    //   }),
+    //   y: dcsdcData.map((v) => {
+    //     return v.Count;
+    //   }),
+    //   name: 'DCSDC',
+    //   orientation: 'v',
+    //   type: 'bar'
+    // };
+    //
+    // let popBandsData = [donegalPopBands, dcsdcPopBands];
+    // let popBandsLayout = Object.assign({}, groupedColumnLayout);
+    // popBandsLayout.title = 'Population by Age Band';
+    // popBandsLayout.legend = {
+    //   x: 1,
+    //   y: 0.5
+    // };
+    //
+    // Plotly.newPlot('pop-bands-chart', popBandsData, popBandsLayout, {
+    //   modeBarButtonsToRemove: modeBarButtonsRemove,
+    //   displayModeBar: true,
+    //   displaylogo: false,
+    //   showSendToCloud: false,
+    //   responsive: true
+    // });
+
+  })
+  .catch(function(err) {
+    console.log("Error loading file\n " + err)
+  });
--- public/javascripts/maps_common.js
+++ public/javascripts/maps_common.js
@@ -1,7 +1,8 @@
-let dubLat = 53.3498;
-let dubLng = -6.2603;
-let min_zoom = 8, max_zoom = 18;
-let zoom = 10;
+let nwLat = 54.92;
+let nwLng = -8;
+let min_zoom = 8,
+  max_zoom = 18;
+let zoom = 9;
 // tile layer with correct attribution
 let osmUrl = 'http://{s}.tile.openstreetprivateMap.org/{z}/{x}/{y}.png';
 let osmUrl_BW = 'http://{s}.tiles.wmflabs.org/bw-privateMapnik/{z}/{x}/{y}.png';
@@ -11,7 +12,11 @@ let stamenTonerUrl_Lite = 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/
 let osmAttrib = 'Map data © <a href="http://openstreetprivateMap.org">OpenStreetMap</a> contributors';
 let osmAttrib_Hot = '&copy; <a href="http://www.openstreetprivateMap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetprivateMap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>';
 let stamenTonerAttrib = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetprivateMap.org/copyright">OpenStreetMap</a>';
-let iconAX = 15;  //icon Anchor X
+let iconAX = 15; //icon Anchor X
 let iconAY = 15; //icon Anchor Y
 
 
+proj4.defs("EPSG:29902", "+proj=tmerc +lat_0=53.5 +lon_0=-8 +k=1.000035 \n\
++x_0=200000 \n\+y_0=250000 +a=6377340.189 +b=6356034.447938534 +units=m +no_defs");
+var firstProjection = "EPSG:29902";
+var secondProjection = "EPSG:4326";
