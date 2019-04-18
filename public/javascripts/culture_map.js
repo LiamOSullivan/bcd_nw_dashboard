81e5dc3c0a843b2a0c0c904e49c9c69f07dc75fe
Liam O'Sullivan <>
2019-04-18 15:46:19 +0100

    Styles & clustering for map via css

--- public/javascripts/culture_map.js
+++ public/javascripts/culture_map.js
@@ -11,13 +11,8 @@ cultureMap.addLayer(cultureOSM);
 /************************************
  * culture
  ************************************/
-// let cultureCluster = L.markerClusterGroup();
-// let cultureMapIcon = L.icon({
-//   iconUrl: '/images/transport/bicycle-15.svg',
-//   iconSize: [30, 30], //orig size
-//   iconAnchor: [iconAX, iconAY] //,
-//   //popupAnchor: [-3, -76]
-// });
+
+
 
 d3.csv("../data/Cultural_Services_and_Facilities.csv")
   .then(function(data) {
@@ -29,79 +24,34 @@ d3.csv("../data/Cultural_Services_and_Facilities.csv")
     //   return v.Region === "Donegal";
     // });
     //
+
+    let cultureMapIcon = L.icon({
+      iconUrl: '/images/marker-15.svg',
+      iconSize: [30, 30], //orig size
+      iconAnchor: [15, 30],
+      popupAnchor: [0, -30]
+    });
+
+    let cultureCluster = L.markerClusterGroup();
     cultureData = data.map((d) => {
       let result = proj4(firstProjection, secondProjection, [+d["EASTING"], +d["NORTHING"]]);
-      let marker = L.marker(new L.LatLng(result[1], result[0]))
-      // , {
-      //   icon: waterLevelMapIcon
-      // });
-      marker.bindPopup(getPopupInfo(d));
-      cultureMap.addLayer(marker);
+      cultureCluster.addLayer(L.marker(new L.LatLng(result[1], result[0]), {
+          icon: cultureMapIcon
+        })
+        .bindPopup(getPopupInfo(d)));
     });
+    cultureMap.addLayer(cultureCluster);
+    cultureMap.fitBounds(cultureCluster.getBounds());
 
     // console.log("Mapped data " + JSON.stringify(cultureData[0]));
 
     function getPopupInfo(d_) {
-      return d_["NAME"] + "<br>" +
-        d_["TYPE"] + "<br>" +
+      return '<h3>' + d_["NAME"] + '</h3>' +
+        '<b><i>' + d_["TYPE"] + '</i></b><br>' +
+        d_["ARTFORM"] + "<br>" +
         d_["ADDRESS"] + "<br>" +
         d_["WEB"] + "<br>";
-
     }
-
-    //
-    // let dcsdcData = data.filter((v) => {
-    //   return v.Region === "DCSDC";
-    // });
-    //
-    // dcsdcData = dcsdcData.map((v) => {
-    //   return {
-    //     Band: v["Age band"],
-    //     Count: parseFloat(v["Count"].replace(/,/g, ''))
-    //   }
-    // });
-    //
-    // // console.log("Mapped DCSDC data " + JSON.stringify(dcsdcData[0]));
-    //
-    // let donegalPopBands = {
-    //   x: donegalData.map((v) => {
-    //     return v.Band;
-    //   }),
-    //   y: donegalData.map((v) => {
-    //     return v.Count;
-    //   }),
-    //   name: 'Donegal',
-    //   orientation: 'v',
-    //   type: 'bar'
-    // };
-    //
-    // let dcsdcPopBands = {
-    //   x: dcsdcData.map((v) => {
-    //     return v.Band;
-    //   }),
-    //   y: dcsdcData.map((v) => {
-    //     return v.Count;
-    //   }),
-    //   name: 'DCSDC',
-    //   orientation: 'v',
-    //   type: 'bar'
-    // };
-    //
-    // let popBandsData = [donegalPopBands, dcsdcPopBands];
-    // let popBandsLayout = Object.assign({}, groupedColumnLayout);
-    // popBandsLayout.title = 'Population by Age Band';
-    // popBandsLayout.legend = {
-    //   x: 1,
-    //   y: 0.5
-    // };
-    //
-    // Plotly.newPlot('pop-bands-chart', popBandsData, popBandsLayout, {
-    //   modeBarButtonsToRemove: modeBarButtonsRemove,
-    //   displayModeBar: true,
-    //   displaylogo: false,
-    //   showSendToCloud: false,
-    //   responsive: true
-    // });
 
   })
   .catch(function(err) {
