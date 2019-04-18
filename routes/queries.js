052c59b7251ca165e1ca7634ad2fe2be4482e38a
Liam O'Sullivan <>
2019-04-18 16:31:46 +0100

    Prune codebase

--- routes/queries.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET themes page. */
-router.get('/', function(req, res, next) {
-  res.render('queries', { title: 'Queries Page' });
-});
-
-module.exports = router;
